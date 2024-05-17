394326780c2a36e938341b5cb053c1ca6242c89c
alexcuadroo <com.alexcuadro@gmail.com>
2024-05-16 23:36:37 -0300

    se refactoriza las funciones de js y se separa el array de las funciones, creando asi el data.js

---
+++ components/data.js
@@ -0,0 +1,48 @@
+export const tarjetas = [
+    {
+        fecha: "10 de mayo de 2024 - IP nueva: noobalex.enxada.host",
+        card1: "actual",
+        pastilla1: "Nuevo",
+        descargarSi: "si",
+        instrucciones: [
+            "Borra todos los mods de la carpeta mods",
+            "Descomprime la carpeta descargada y agrega los nuevos mods"
+        ],
+        descarga: "../data-mc/mods mayo.zip",
+        notas: [
+            "Se ha agregado el mod biggerstacks que estaba en la versión anterior",
+            "Nueva ip: noobalex.enxada.host",
+            "Nos mudamos a Brasil"
+        ],
+    },
+    {
+        fecha: "5 de mayo de 2024",
+        card1: "pasado",
+        pastilla1: "Antiguo",
+        descargarSi: "no",
+        instrucciones: [
+            "Borra todos los mods de la carpeta mods",
+            "Descomprime la carpeta descargada y agrega los nuevos mods",
+        ],
+        descarga: "#",
+        notas: [
+            "Se han actualizado todos los mods",
+            "Se han borrado mods no usados e incorporado +4 de estructuras",
+            "Se ha regenerado el mapa explorado",
+        ],
+    },
+    {
+        fecha: "1 de mayo de 2024",
+        card2: "pasado",
+        pastilla2: "Antiguo",
+        descargarNo: "no",
+        instrucciones: [
+            "Borra el mod de player music y uteam core",
+            "Coloca después de descomprimir los descargados",
+        ],
+        descarga: "#",
+        notas: [
+            "Se corrige error del mod actualizando dos versiones",
+        ],
+    },
+];
