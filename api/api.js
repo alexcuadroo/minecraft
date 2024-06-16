8de99d7f5ec3c901dab136640f0bd6b5697e65d1
alexcuadroo <com.alexcuadro@gmail.com>
2024-06-16 03:00:14 -0300

    cambios en la rama dev con respecto a la api, evitando llamarla dos veces

---
+++ api/api.js
@@ -0,0 +1,31 @@
+async function fetchData() {
+    try {
+        const response = await fetch("https://api-mc.vercel.app/api/status"); // Ajusta la ruta según donde esté desplegada tu API
+        const data = await response.json();
+
+
+        // Actualizar los elementos HTML con los datos obtenidos
+        document.getElementById("status").textContent = data.online
+            ? "En línea"
+            : "Apagado";
+        document.getElementById("version").textContent =
+            data.version.name_clean;
+        document.getElementById("protocol").textContent =
+            data.version.protocol;
+        document.getElementById("players").textContent =
+            data.players;
+        document.getElementById("player-list").textContent =
+            data.playerNames.join(", ");
+    } catch (error) {
+        console.error("Error al obtener datos:", error);
+    }
+}
+
+// Función para actualizar los datos cada 30 segundos
+function updateDataPeriodically() {
+    fetchData(); // Primera llamada al cargar la página
+    setInterval(fetchData, 30000); // Llama a fetchData cada 30 segundos (30000 milisegundos)
+}
+
+// Iniciar la actualización periódica al cargar la página
+updateDataPeriodically();
